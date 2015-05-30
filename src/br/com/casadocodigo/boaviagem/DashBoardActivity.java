20ad2577d093138e00c912e637520d51d93f2c9d
Francisco André <fandrefh@gmail.com>
2015-05-30 11:48:27 -0300

    Subindo o projeto

---
+++ src/br/com/casadocodigo/boaviagem/DashBoardActivity.java
@@ -0,0 +1,26 @@
+package br.com.casadocodigo.boaviagem;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class DashBoardActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_dashboard);
+	}
+	
+	public void selecionarOpcao(View view) {
+		switch (view.getId()) {
+		case R.id.novo_gasto:
+			startActivity(new Intent(this, GastoActivity.class));
+			break;
+
+		default:
+			break;
+		}
+	}
+}
